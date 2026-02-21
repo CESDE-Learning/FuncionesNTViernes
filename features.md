8a1715ced95d50c12ad1f7fe81d49122c06ee3a2
TasyDev <tamayocontat@gmail.com>
2026-02-20 20:08:22 -0500

    Add: Primera funcion en pyton

--- features.md
+++ features.md
@@ -8,4 +8,13 @@
 
 ### New file: Bicletras
 
-## Que bicicleta gano
+### Que bicicleta gano
+
+#### Una lista de estudiantes
+#### Una lista de 20 calificaciones de eficicencia
+#### Una lista de 20 calificaciones de aparencia
+#### Una lista de 20 califiaciones de estabilidad
+
+### Calcular el promedio de las notas
+
+### Evaluar la bicilita ganadora
---
+++ funcionesUno.py
@@ -0,0 +1,23 @@
+# Declarar funciones
+
+def crearListasEstudiantes(cantidadEstudiantes):
+    estudiantes=[]
+    for i in range(cantidadEstudiantes):
+        estudiante={
+
+        }
+        estudiantes=["id"]=input("id: ")
+        estudiantes=["nombre"]=input("nombre: ")
+        estudiantes=["documento"]=input("documento: ")
+        estudiantes=["correo"]=input("correo: ")
+        estudiantes=["telefono"]=input("telefono: ")
+        estudiantes=["telefono"]=input("telefono: ")
+        estudiantes=["semestre"]=input("semestre: ")
+        estudiantes=["becado"]=input("becado: ")
+        estudiante.append(estudiantes)
+
+    return estudiante
+
+#Llamado de funcion            
+resultado=crearListasEstudiantes(2)
+print(resultado)
